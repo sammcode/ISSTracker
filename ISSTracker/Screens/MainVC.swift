033aa93a23c407085644386bc6fc77ce4765a5e9
sammcode <samuel.mcgarry@gmail.com>
2021-05-11 12:47:58 -0400

    Removed unused code in MainVC

--- ISSTracker/Screens/MainVC.swift
+++ ISSTracker/Screens/MainVC.swift
@@ -17,19 +17,12 @@ class MainVC: ITDataLoadingVC {
     /// This helps create the perception that there are an infinite loop of images
     let imageData = [Images.iss8, Images.iss1, Images.iss2, Images.iss3, Images.iss4, Images.iss5, Images.iss6, Images.iss7, Images.iss8, Images.iss1]
 
-    var initalScroll = false
     let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
-    var collectionView: UICollectionView!
-    var locationManager = CLLocationManager()
-    var userLocation = CLLocationCoordinate2D(latitude: 45.570195, longitude: -122.825434)
     let generator = UINotificationFeedbackGenerator()
 
-    var imagesDescriptionLabel = ITDescriptionLabel(textAlignment: .center, fontSize: 14)
     var buttonsStackView = UIStackView()
     var trackISSButton = ITButton(backgroundColor: Colors.mainBlueYellow, title: "Track ISS")
-    var trackISSDescriptionLabel = ITDescriptionLabel(textAlignment: .center, fontSize: 14)
     var predictPassesButton = ITButton(backgroundColor: Colors.mainBlueYellow, title: "Predict Pass Times")
-    var predictPassesDescriptionLabel = ITDescriptionLabel(textAlignment: .center, fontSize: 14)
     var peopleInSpaceButton = ITButton(backgroundColor: Colors.mainBlueYellow, title: "People In Space")
     var titleLabel = ITTitleLabel(textAlignment: .center, fontSize: 48)
 
@@ -110,19 +103,11 @@ class MainVC: ITDataLoadingVC {
         playVideo()
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        if !self.initalScroll {
-            self.initalScroll = true
-            self.collectionView.scrollToItem(at: IndexPath(item: 1, section: 0), at: .centeredHorizontally, animated: false)
-        }
-    }
-
     /// Checks if the app has been launched before
     /// If not, the InfoVC ViewController is presented to the user
     func checkIfFirstLaunch(){
         if !launchedBefore {
-            presentInfoVC()
+            presentSettingsVC()
             UserDefaults.standard.set(true, forKey: "launchedBefore")
         }
     }
@@ -139,19 +124,9 @@ class MainVC: ITDataLoadingVC {
     func configureViewController(){
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
-        let infoButton = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain, target: self, action: #selector(presentInfoVC))
+        let infoButton = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain, target: self, action: #selector(presentSettingsVC))
         navigationItem.rightBarButtonItem = infoButton
 
-    }
-
-    /// Requests authorization for accessing users location; if granted access, sets properties for location manager, starts updating the users current location
-    func configureLocationManager(){
-        locationManager.requestWhenInUseAuthorization()
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-        }
     }
 
     /// Configures the title label, constains it to the top of the view
@@ -167,35 +142,6 @@ class MainVC: ITDataLoadingVC {
             titleLabel.heightAnchor.constraint(equalToConstant: 50),
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height * 0.1)
         ])
-    }
-
-    /// Configures the image description label, constrains it to the bottom of the collection view
-    func configureImagesDescriptionLabel(){
-        view.addSubview(imagesDescriptionLabel)
-        NSLayoutConstraint.activate([
-            imagesDescriptionLabel.topAnchor.constraint(equalTo: collectionView.bottomAnchor),
-            imagesDescriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imagesDescriptionLabel.widthAnchor.constraint(equalToConstant: 200),
-            imagesDescriptionLabel.heightAnchor.constraint(equalToConstant: 20)
-        ])
-        imagesDescriptionLabel.text = "Recent photos from the ISS"
-    }
-
-    /// Configures the button description labels, constrains them to the bottom of their respective buttons
-    func configureButtonDescriptionLabels(){
-        NSLayoutConstraint.activate([
-            trackISSDescriptionLabel.heightAnchor.constraint(equalToConstant: 40),
-            trackISSDescriptionLabel.widthAnchor.constraint(equalToConstant: 240)
-        ])
-        trackISSDescriptionLabel.numberOfLines = 0
-        trackISSDescriptionLabel.text = "Track the current location of the International Space Station"
-
-        NSLayoutConstraint.activate([
-            predictPassesDescriptionLabel.heightAnchor.constraint(equalToConstant: 40),
-            predictPassesDescriptionLabel.widthAnchor.constraint(equalToConstant: 240)
-        ])
-        predictPassesDescriptionLabel.numberOfLines = 0
-        predictPassesDescriptionLabel.text = "Predict times when the ISS will pass over your location"
     }
 
     /// Adds actions to the buttons, sets the width and height for both of them
@@ -279,49 +225,6 @@ class MainVC: ITDataLoadingVC {
         }
     }
 
-    /// Uses the NetworkManager class to get predicted pass times based on the user's location
-    /// If location authorization has been denied, a custom alert is presented prompting the user to adjust their settings
-    /// If location authorization has not been determined, the location manager requests for authorization from the user
-    /// If location authorization is allowed by the user when the app is in use, the NetworkManager class attempts to retrieve the data
-    /// On success, an instance of PredictPassesVC ViewController is presented with the retrieved data
-    /// On failure, a custom alert is presented stating the error in question
-    @objc func getPassTimes(){
-        predictPassesButton.pulsate()
-        if locationManager.authorizationStatus == .denied {
-            self.presentITAlertOnMainThread(title: "Location Access Disabled", message: ITError.locationServicesTurnedOff.rawValue, buttonTitle: "Ok", settingsButtonNeeded: true)
-            return
-        }else if locationManager.authorizationStatus == .notDetermined {
-            locationManager.requestWhenInUseAuthorization()
-        }
-
-        if locationManager.authorizationStatus == .authorizedWhenInUse {
-            showLoadingView()
-
-            NetworkManager.shared.getISSPasstimes(latitude: userLocation.latitude, longitude: userLocation.longitude) { [weak self] result in
-
-                guard let self = self else { return }
-
-                self.dismissLoadingView()
-
-                switch result {
-                case .success(let passes):
-                    if UserDefaultsManager.haptics { self.generator.notificationOccurred(.success) }
-                    DispatchQueue.main.async {
-                        let predictPassesVC = PredictPassesVC()
-                        predictPassesVC.userLocation = self.userLocation
-                        predictPassesVC.passTime = passes
-                        let navController = UINavigationController(rootViewController: predictPassesVC)
-
-                        self.present(navController, animated: true)
-                    }
-                case .failure(let error):
-                    if UserDefaultsManager.haptics { self.generator.notificationOccurred(.error) }
-                    self.presentITAlertOnMainThread(title: "Oh no!", message: error.rawValue, buttonTitle: "Ok")
-                }
-            }
-        }
-    }
-
     @objc func getPeopleInSpace(){
         peopleInSpaceButton.pulsate()
         showLoadingView()
@@ -329,21 +232,15 @@ class MainVC: ITDataLoadingVC {
             guard let self = self else { return }
             self.dismissLoadingView()
             switch result {
-            case .success(let people):
-
-                print("GOT HERE")
-                people.people.forEach { (person) in
-                print("Person: \(person.name)")
-                print("Craft: \(person.craft)")
+            case .success(let peopleData):
                 if UserDefaultsManager.haptics { self.generator.notificationOccurred(.success) }
                 DispatchQueue.main.async {
                     let peopleInSpaceVC = PeopleInSpaceVC()
-                    peopleInSpaceVC.peopleInSpace = people
+                    peopleInSpaceVC.peopleInSpace = peopleData
                     let navController = UINavigationController(rootViewController: peopleInSpaceVC)
 
                     self.present(navController, animated: true)
                 }
-            }
             case .failure(let error):
                 if UserDefaultsManager.haptics { self.generator.notificationOccurred(.error) }
                 self.presentITAlertOnMainThread(title: "Oh no!", message: error.rawValue, buttonTitle: "Ok")
@@ -351,59 +248,9 @@ class MainVC: ITDataLoadingVC {
         }
     }
 
-    @objc func presentInfoVC(){
-//        let infoVC = InfoVC()
-//        let navController = UINavigationController(rootViewController: infoVC)
-//        self.present(navController, animated: true)
+    @objc func presentSettingsVC(){
         let settingsVC = SettingsVC()
         let navController = UINavigationController(rootViewController: settingsVC)
         self.present(navController, animated: true)
-    }
-}
-
-extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
-    /// Returns the number of items in the collection view section, which is set to the number of images in the imageData array
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageData.count
-    }
-
-    /// Returns the custom collection view cell to be used, and sets the image based on the indexPath
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ITImageCell.reuseID, for: indexPath) as! ITImageCell
-        let image = imageData[indexPath.row]
-        cell.set(img: image!)
-        return cell
-    }
-
-    /// Returns the edge insets for the collection view section
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-    }
-
-    /// When the scroll view stops decelerating, the current cell is calculated by dividing the scroll view contentOffset by the scroll view width
-    /// If the current cell int (index) is 0, the scroll view scrolls to the last item in the array
-    /// If the current cell int (index) is 7, the scroll view scrolls to the second item in the array (which appears to be the first)
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let currentCellFloat = (scrollView.contentOffset.x / scrollView.frame.size.width)
-        let currentCellInt = Int(round(currentCellFloat))
-
-        switch currentCellInt {
-        case 0:
-            collectionView.scrollToItem(at: [0, 8], at:  .left, animated: false)
-        case 7:
-            collectionView.scrollToItem(at: [0, 1], at: .right, animated: false)
-        default:
-            break
-        }
-    }
-}
-
-extension MainVC: CLLocationManagerDelegate {
-
-    /// When the user's location is updated, the new coordinates are assigned to the userLocation variable
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let coords: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        userLocation = coords
     }
 }
